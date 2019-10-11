c0740da2e64f82be88736c7e1d3327696d1b03aa
jayabalandevsecops <jayabalandevops@gmail.com>
2019-10-11 08:59:44 +0530

    added parameters clause

--- cpl.groovy
+++ cpl.groovy
@@ -1,17 +1,19 @@
 folder('$BUName'){}
 folder('$BUName/$ProductName'){}
 pipelineJob('$BUName/$ProductName/CICD_$AppName'){
-    stringParam("AppRepo", "$ApplicationRepo", "Git URL")
-    stringParam("AppName", "$AppName", "")
-    stringParam("UnitTestTool", "$UnitTestRun","")
-    stringParam("NodeName", "$NodeName", "")
-    activeChoiceParam('Branch'){
-        filterable()
-        description("Select the Branch to be built...")
-        choiceType('SINGLE_SELECT')
-        groovyScript{
-            script('["Select","master","develop"]')
-            fallbackScript('"Fallback choice"')
+    parameters{
+        stringParam("AppRepo", "$ApplicationRepo", "Git URL")
+        stringParam("AppName", "$AppName", "")
+        stringParam("UnitTestTool", "$UnitTestRun","")
+        stringParam("NodeName", "$NodeName", "")
+        activeChoiceParam('Branch'){
+            filterable()
+            description("Select the Branch to be built...")
+            choiceType('SINGLE_SELECT')
+            groovyScript{
+                script('["Select","master","develop"]')
+                fallbackScript('"Fallback choice"')
+            }
         }
     }
     definition{
