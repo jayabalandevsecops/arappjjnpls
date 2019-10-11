bc8ac69075c4669b943daffd7f07d29498df4f82
jayabalandevsecops <jayabalandevops@gmail.com>
2019-10-11 08:49:53 +0530

    added cpl and touched cit groovy

---
+++ cpl.groovy
@@ -0,0 +1,25 @@
+folder('$BUName'){}
+folder('$BUName/$ProductName'){}
+pipelineJob('$BUName/$ProductName/CICD_$AppName'){
+    stringParam("AppRepo", "$ApplicationRepo", "Git URL")
+    stringParam("AppName", "$AppName", "")
+    stringParam("UnitTestTool", "$UnitTestRun","")
+    stringParam("NodeName", "$NodeName", "")
+    activeChoiceParam('Branch'){
+        filterable()
+        description("Select the Branch to be built...")
+        choiceType('SINGLE_SELECT')
+        groovyScript{
+            script('["Select","master","develop"]')
+            fallbackScript('"Fallback choice"')
+        }
+    }
+    definition{
+        cps{
+            def jobScript = readFileFromWorkspace('cit.groovy')
+            def approvals = org.jenkinsci.plugins.scriptsecurity.scripts.ScriptApproval.get()
+            approvals.approveScript(approvals.hash(jobScript, "groovy"))
+            script(jobScript)
+        }
+    }
+}
